f9b477721047a6e2072dcbd222b13654a6d905b1
LigitasSpo <ligitas.spo@gmail.com>
2019-03-27 13:17:43 +0200

    PHP Courses

--- OOP/index.php
+++ OOP/index.php
@@ -3,6 +3,8 @@ require_once 'classes/Product.php';
 require_once 'classes/Book.php';
 require_once 'classes/count.php';
 require_once 'classes/Calculator.php';
+require_once 'classes/Database.php';
+
 $book = new Book('Children', 8.99,'123456789');
 
 echo $book->calculateProductVAT();
@@ -23,5 +25,22 @@ $calc->substract(2);
 
 
 echo $calc->getTotal();
+
+echo '<br>';
+
+
+$db = new Database('localhost','root','','test');
+$db->query("SELECT `articles`. `title`,`articles`. `id` FROM `articles`");
+
+
+if ($db->numRows() == 0) {
+    echo 'No articles';
+} else {
+    foreach ($db->rows() as $article) {
+        echo $article['id']. '-' . $article['title']. '<br>';
+
+    }
+     echo '<p>'. $db->numRows(), 'Articles </p>';
+}
 
 ?>
